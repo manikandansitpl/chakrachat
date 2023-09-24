5bf3c54b890bbe4cd7c0aab809152cf68dd7ac0b
maniadminm <multidiscoverserver@gmail.com>
2023-09-25 01:57:05 +0530

    msg

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const colors = require("colors");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect("mongodb+srv://manikandansitpl3:83mb1dmaopS%40@mobilecluster0.nkmpjqj.mongodb.net/?retryWrites=true&w=majority", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: true,
