8704a1444be83da03fb950b101e54a01729c0ee6
maniadminm <multidiscoverserver@gmail.com>
2023-09-25 01:26:41 +0530

    SERVER

--- backend/server.js
+++ backend/server.js
@@ -53,7 +53,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://172.105.40.36:5000",
     // credentials: true,
   },
 });
